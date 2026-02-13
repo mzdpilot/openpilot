8788727e4946ce6c5850d4c713a650e06388117d
MazdaNick <nick.tompk@gmail.com>
2026-02-13 13:39:17 -0500

    prioritize CAN ignition

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -19,6 +19,8 @@ from openpilot.system.athena.registration import register, UNREGISTERED_DONGLE_I
 from openpilot.common.swaglog import cloudlog, add_file_handler
 from openpilot.system.version import get_build_metadata, terms_version, training_version
 from openpilot.system.hardware.hw import Paths
+from openpilot.system.hardware import PC
+from openpilot.system.hardware.ignition_state import ignition_state
 
 
 def manager_init() -> None:
@@ -144,7 +146,7 @@ def manager_thread() -> None:
     elif not started and started_prev:
       params.clear_all(ParamKeyFlag.CLEAR_ON_OFFROAD_TRANSITION)
 
-    ignition = any(ps.ignitionLine or ps.ignitionCan for ps in sm['pandaStates'] if ps.pandaType != log.PandaState.PandaType.unknown)
+    ignition = ignition_state.update(sm['pandaStates'])
     if ignition and not ignition_prev:
       params.clear_all(ParamKeyFlag.CLEAR_ON_IGNITION_ON)
 
