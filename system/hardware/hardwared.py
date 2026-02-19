fdd135e4ade6203ce1bfa7cd67090867ead14336
MazdaNick <nick.tompk@gmail.com>
2026-02-19 18:32:17 -0500

    clean hardwared.py imports

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -24,7 +24,7 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.system.hardware.power_monitoring import PowerMonitoring
 from openpilot.system.hardware.fan_controller import TiciFanController
 from openpilot.system.hardware.ignition_state import ignition_state
-from openpilot.system.version import terms_version, training_version, get_build_metadata, terms_version_sp
+from openpilot.system.version import terms_version, training_version, get_build_metadata
 
 
 ThermalStatus = log.DeviceState.ThermalStatus
