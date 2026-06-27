fdabdb2fa21eee2571549d1dfc90fd7fadd29438
MazdaNick <nick.tompk@gmail.com>
2026-06-26 20:10:01 -0400

    Revert "ignition: prioritize CAN over ignitionLine"
    This reverts commit 7d7f1e8dd91de59b6d5cb5969c8db1a3d83ad5cd.

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -23,7 +23,6 @@ from openpilot.system.statsd import statlog
 from openpilot.common.swaglog import cloudlog
 from openpilot.system.hardware.power_monitoring import PowerMonitoring
 from openpilot.system.hardware.fan_controller import FanController
-from openpilot.common.ignition import get_ignition_state
 from openpilot.system.version import terms_version, training_version, get_build_metadata, terms_version_sp
 
 ThermalStatus = log.DeviceState.ThermalStatus
@@ -213,7 +212,7 @@ def hardware_thread(end_event, hw_queue) -> None:
     if sm.updated['pandaStates'] and len(pandaStates) > 0:
 
       # Set ignition based on any panda connected
-      onroad_conditions["ignition"] = get_ignition_state(pandaStates)
+      onroad_conditions["ignition"] = any(ps.ignitionLine or ps.ignitionCan for ps in pandaStates if ps.pandaType != log.PandaState.PandaType.unknown)
 
       pandaState = pandaStates[0]
 
